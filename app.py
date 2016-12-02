241ccbc93f476403c92a178dc0a0b6e4465f79a8
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2016-12-02 14:35:08 +0100

    try

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'jay'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'jay'
-app.config['MYSQL_DATABASE_DB'] = 'BucketList'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'start'
+app.config['MYSQL_DATABASE_DB'] = 'py'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
